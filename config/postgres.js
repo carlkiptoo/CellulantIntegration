4663bb61df46a1488040ddbbf2cd63acd886a5c5
carlkiptoo <carlosiamcarl@gmail.com>
2025-10-25 18:32:55 +0300

    Table creation

--- config/postgres.js
+++ config/postgres.js
@@ -12,10 +12,27 @@ const pool = new Pool({
     port: process.env.PG_PORT || 5432,
 });
 
-const connectDB = async () => {
+const connectDB = async (createTables = true) => {
     try {
         await pool.connect();
         console.log('Connected to PostgreSQL database');
+        if (createTables) {
+            const client = await pool.connect();
+
+            try {
+                const createStudentsTable = (await import('../db/student.model.js')).default;
+                const createPaymentsTable = (await import('../db/payment.model.js')).default;
+
+                await client.query(createStudentsTable);
+                console.log('Students table ensured');
+                
+                await client.query(createPaymentsTable);
+                console.log('Payments table ensured');
+            } catch (error) {
+                console.error('Error creating tables:', error);
+                process.exit(1);
+            }
+        }
     } catch (error) {
         console.error('Error connecting to PostgreSQL database:', error);
         process.exit(1);
