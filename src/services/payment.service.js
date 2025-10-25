4d9612814c967061572a1ac93983a71789b79e5e
carlkiptoo <carlosiamcarl@gmail.com>
2025-10-26 01:28:39 +0300

    Payment processing logic

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -6,20 +6,58 @@ export const isTransactionProcessed = async (transactionId) => {
     const result = await pool.query(query, [transactionId, 'SUCCESS']);
     return result.rows.length > 0;
 };
-/**
- * Handles the full payment transaction (Idempotency check, record, update balance).
- * NOTE: This will be implemented fully later using a PostgreSQL transaction.
- */
 
 export const processNewPayment = async (paymentData) => {
-    logger.log(`Processing new payment: ${JSON.stringify(paymentData.transactionId)}`, 'Worker task');
+  const {transactionId, studentId, amountPaid, paymentMethod, status, timestamp} = paymentData;
 
-    // Placeholder logic for now:
-    // 1. START Transaction
-    // 2. Insert into Payments table
-    // 3. Update Students balance
-    // 4. COMMIT Transaction
+  if (status !== 'SUCCESS') {
+    logger.log(`Skipping transaction ${transactionId} with status ${status}`, 'Worker task');
+    return false;
+  }
+
+  const client = await pool.connect();
+
+  try {
+    await client.query('BEGIN');
+
+    const processed = await isTransactionProcessed(transactionId);
+    if  (processed) {
+        logger.log(`Transaction ${transactionId} already processed`, 'Worker task');
+        await client.query('ROLLBACK');
+        return true;
+    }
+
+    const insertPaymentQuery = `
+    INSERT INTO payments (transaction_id, student_id, amount_paid, payment_method, status, payment_date, cellulant_ref)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `;
     
-    // In the final implementation, this should return a status after the transaction.
+    const paymentValues = [
+        transactionId,
+        studentId,
+        amountPaid,
+        paymentMethod,
+        status,
+        timestamp
+    ];
+    await client.quuery(insertPaymentQuery, paymentValues);
+
+    const updateStudentQuery = `
+        UPDATE students
+        SET current_balance = current_balance - $1,
+            updated_at = NOW()
+        WHERE student_id = $2;
+        `;
+    await client.query(updateStudentQuery, [amountPaid, studentId]);
+
+    await client.query('COMMIT');
+    logger.log(`Transaction ${transactionId} successfully recorded and balance updated`, 'Worker task');
     return true;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    logger.error(`Error processing transaction ${transactionId}: ${error.message}`, 'Worker task');
+    throw new Error('Error processing transaction');
+  } finally {
+    client.release();
+  }
 }
