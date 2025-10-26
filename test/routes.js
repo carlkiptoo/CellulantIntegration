5c07f76d92777d7b6759975aaa6ad949e178e10d
carlkiptoo <carlosiamcarl@gmail.com>
2025-10-26 14:01:39 +0300

    protect sim endpoints

--- test/routes.js
+++ test/routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { simulateValidation, simulateWebhook } from "./simulator.controller.js";
+import {verifyBearerToken} from '../src/middleware/auth.middleware.js';
 
 const router = express.Router();
-
+router.use(verifyBearerToken);
 router.post('/simulate/validation/:studentId', simulateValidation);
 router.post('/simulate/webhook/:studentId/:status', simulateWebhook);
 
